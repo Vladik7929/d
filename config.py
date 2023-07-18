39d9443852205de263675fd9223c14f2656d93b7
Vladik7929 <104212489+Vladik7929@users.noreply.github.com>
2023-07-18 14:41:16 +0300

    Create config.py

---
+++ config.py
@@ -0,0 +1,13 @@
+import asyncio
+TOKEN_API = '6322175401:AAEFXOVrIy63gVgwK2HkBUC_4iOLkmq-SoQ'
+HELP_INFO = '''
+/start - начать работу с ботом
+/help - помощь
+/description - описание
+/greet - поприветствовать
+'''
+DESCRIPTION_INFO = '''
+Этот бот работает с Chat Gpt
+'''
+async def greet():
+    print('Привет')
